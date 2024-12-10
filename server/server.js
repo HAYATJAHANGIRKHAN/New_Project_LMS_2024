cb215f33ce9c90646b2111b128e93a8dc511735b
Hayat <hayatjahangir0@gmail.com>
2024-12-10 08:43:36 +0530

    update in server.js port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const {confiq, config} =require('dotenv');
 config();
 const app = require('./app');
-const PORT = process.env.PROCESS || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => {
  console.log(`APP is running at http://localhost:${PORT}`);
 });
