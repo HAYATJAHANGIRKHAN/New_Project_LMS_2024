01d27b65a65297be9b820f2a3263951e6ad82ab2
Hayat <hayatjahangir0@gmail.com>
2024-12-11 19:49:10 +0530

    create user.controller.js export and define how to control routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { getProfile, login, logout, register } from "../controllers/user.controller";
 
 const router = Router();
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', register);
+router.post('/logout', logout);
 router.post('/me', getProfile);
 
 
