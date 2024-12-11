c38a5da2add4267dd6995e70735a9fad82556d8b
Hayat <hayatjahangir0@gmail.com>
2024-12-11 19:41:19 +0530

    craete user.routes.js define routers which we use

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
 
 const router = Router();
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', register);
+router.post('/me', getProfile);
+
 
 export default router;
