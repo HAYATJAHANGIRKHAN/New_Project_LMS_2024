828a541c4cbc2485d312f7772220e3765159f45b
Hayat <hayatjahangir0@gmail.com>
2024-12-11 20:33:42 +0530

    create user.model.js in which we describe all schema which we use in this project

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,8 +2,54 @@ import { Schema, model } from "mongoose";
 
 
 const userSchema = new Schema({
+  fullName:{
+    type: 'String',
+    required:[true, 'Name is required'],
+    minLength: [5,'Name is required must be at least 5 characters'],
+    maxLenth: [50,'Name is required must be at lessthan 50 characters'],
+    lowercase: true,
+    trim: true,
 
-});
+  },
+  email:{
+     type:'String',
+     required:[true, 'Email is required'],
+     unique: true,
+     lowercase: true,
+     trim: true,
+  },
+     
+  password:{
+    type:'String',
+    required:[true, 'Password is required'],
+    minLength: [8,'Password must be at least 8 characters'],
+    trim: true,
+    select: false,
+  },
+  avatar:{
+    public_id: {
+        type: String,
+        
+    },
+    secure_url:{
+      type: String,
+    }
+  },
+  role:{
+  type: String,
+  enum: ['USER', 'ADMIN'], 
+  default: 'USER',
+
+  },
+  forgotPasswordToken: String,
+  forgotPasswordExpiry: Date,
+
+
+},{
+
+    timestamps:true
+}
+);
 
 const user = model('User, userSchema');
 
