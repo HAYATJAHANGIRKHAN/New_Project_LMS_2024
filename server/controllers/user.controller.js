076855edbc55261b32afcb0efce26eb78c6f4622
Hayat <hayatjahangir0@gmail.com>
2024-12-13 19:09:51 +0530

    update user  create user  and also error in user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,29 +1,51 @@
 
 import AppError from "../utils/error.util";
-const register = (req,res,next)=> {
+const register = async (req, res, next) => {
   const { fullName, email, password } = req.body;
 
-  if(!fullName || !email || !password){
-    return next(new AppError('All fields are required',400));
+  if (!fullName || !email || !password) {
+    return next(new AppError('All fields are required', 400));
   }
+  const userExists = await User.findOne({ email: email, password: password });
+  if (userExists) {
+    return next(new AppError('Email already exists', 400));
+  }
+
+  const User = await User.create({
+    fullName,
+    email,
+    password,
+    avatar: {
+      public_id: email,
+      secure_url: 'https://'
+    }
+  });
+
+  if (!User) {
+    return next(new AppError('Failed to register user', 500));
+  }
+  // TODO:FILE UPLOAD 
+  await User.save();
+
+
 };
 
-const login =(req,res)=> {
+const login = (req, res) => {
 
 };
 
-const logout =(req,res)=> {
+const logout = (req, res) => {
 
 };
 
-const getProfile = (req,res)=> {
+const getProfile = (req, res) => {
 
 };
 
 export {
-    register,
-    login,
-    logout,
-    getProfile
- };
+  register,
+  login,
+  logout,
+  getProfile
+};
 
