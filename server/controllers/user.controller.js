22f46324074a39d29d07ecdff9f357e7b7b8f962
Hayat <hayatjahangir0@gmail.com>
2024-12-14 19:04:01 +0530

    update  user password

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -44,12 +44,20 @@ const register = async (req, res, next) => {
     message:'user Successfully registered',
     user,
   })
-
-
 };
 
-const login = (req, res) => {
+const login = async (req, res) => {
+  const {email, password} = req.body;
 
+  if(!email|| !password){
+    return next(new AppError('All fields are required',400));
+  } 
+
+const user = await User.findOne({
+  email
+}).select('+password');
+if(!user || !user. comparePassword(password)){
+  return next(new AppError('All fields are required',400));
 };
 
 const logout = (req, res) => {
