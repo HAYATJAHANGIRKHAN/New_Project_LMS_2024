adbde829d9ab7a55f8961211d5562ca6e4ad1675
Hayat <hayatjahangir0@gmail.com>
2024-12-15 20:52:53 +0530

    update Cloudinary

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import { v2 } from 'cloudinary';
+import Cloudinary from 'cloudinary';
 import Razorpay from 'razorpay';
  import app from './app.js'; // Import the app instance
  import { config } from 'dotenv';
@@ -17,7 +17,7 @@ import Razorpay from 'razorpay';
 
 
 // Cloudinary configuration
-v2.config({
+Cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
